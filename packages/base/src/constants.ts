2df737078a4c785f6db4c994e95fe9c99ec33863
Ervasive <ervasive@gmail.com>
2020-02-26 00:34:45 +0200

    feat(base): add babel-plugin-intl to configuration

--- packages/base/src/constants.ts
+++ packages/base/src/constants.ts
@@ -1,12 +1,11 @@
+import path from 'path'
 export const PLUGIN_NAME = '@gatsby-plugin-multilingual/base'
 export const PREFIX = '__gpml'
 export const MESSAGE_NODE_TYPENAME = 'GPMLMessage'
 export const TRANSLATION_NODE_TYPENAME = 'GPMLTranslation'
 
-/**
- * Unique name for message nodes
- */
-export const MESSAGE_NODE_TYPENAME = 'GPMLMessage'
+export const CACHE_DIR = path.resolve('.cache', PREFIX)
+export const EXTRACTED_MESSAGES_DIR = path.join(CACHE_DIR, 'messages')
 
 export const DEFAULT_OPTIONS: Options = {
   defaultLanguage: 'en',
--- packages/base/src/gatsby-node.ts
+++ packages/base/src/gatsby-node.ts
@@ -1,6 +1,7 @@
 import { GatsbyNode, CreateSchemaCustomizationArgs } from 'gatsby'
 import { messageTypedef, translationTypedef } from './graphql-types'
 import { validatePluginInstance } from './utils'
+import { EXTRACTED_MESSAGES_DIR } from './constants'
 import { GatsbyStorePlugin } from './types'
 
 /**
@@ -11,6 +12,28 @@ export const createSchemaCustomization: GatsbyNode['createSchemaCustomization'] 
 }: CreateSchemaCustomizationArgs) => {
   actions.createTypes(translationTypedef)
   actions.createTypes(messageTypedef)
+}
+
+/**
+ * Add "babel-plugin-intl" to babel configuration
+ */
+export const onCreateBabelConfig: GatsbyNode['onCreateBabelConfig'] = ({
+  actions,
+}) => {
+  actions.setBabelOptions({
+    options: {
+      // TODO: find a way to invalidate babel-loader cache on deleted messages dir
+      cacheDirectory: false,
+    },
+  })
+  actions.setBabelPlugin({
+    name: 'babel-plugin-react-intl',
+    options: {
+      messagesDir: EXTRACTED_MESSAGES_DIR,
+      extractSourceLocation: true,
+      extractFromFormatMessageCall: true,
+    },
+  })
 }
 
 /**
